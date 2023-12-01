d7608c5423bbd4d557aa22ee3f5c4225758a3ca7
Sai Prakash Gundemeda <113116997+Saiprakashgundemeda@users.noreply.github.com>
2023-12-01 17:28:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import ClickCounter from './components/ClickCounter'
+
+import './App.css'
+
+const App = () => (
+  <div className="bgContainer">
+    <ClickCounter />
+  </div>
+)
+
+export default App
